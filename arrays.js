a5e983d6661956481684073e18b9d014c35f54cf
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-10-27 16:30:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray (array, element) {
-  array = ["food", ...array];
+  array = ["foo", ...array];
   return array;
 }
 
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
 }
 
 function addElementToEndOfArray (array, element) {
-  array = [...array, "food"];
+  array = [...array, "foo"];
   return array;
 }
 
